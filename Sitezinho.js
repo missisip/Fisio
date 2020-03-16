bc1a768adf5dbe08bc8918a10621cd3b3af258bd
missisip <57716070+missisip@users.noreply.github.com>
2020-03-16 17:48:43 -0300

    Add files via upload

---
+++ Sitezinho.js
@@ -0,0 +1,21 @@
+let links = document.querySelectorAll('#bottom-bar a')
+
+for (link of links) {
+	let target = link.getAttribute('data-target')
+
+	link.addEventListener('click', function() {
+		mudaTela(target)
+	})
+}
+
+
+function mudaTela(target) {
+	let tela_target = document.getElementById(target)
+	let link_target = document.querySelector('a[data-target="'+target+'"]')
+
+	document.querySelector('div.tela.active').classList.remove('active')
+	document.querySelector('a.active').classList.remove('active')
+
+	tela_target.classList.add('active')
+	link_target.classList.add('active')
+}
